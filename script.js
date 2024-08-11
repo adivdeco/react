5325f115820914fcc7f4f40ae84acb9523f60393
adivdeco <165708861+adivdeco@users.noreply.github.com>
2024-08-11 23:11:43 +0530

    Add files via upload
    react basic createElement

---
+++ script.js
@@ -0,0 +1,24 @@
+const h2 = React.createElement('h1',{className:'subheadind'},'hloo',
+    React.createElement("section",{className:'container',id:'container'},
+        React.createElement("nav",{},
+        React.createElement("p",{},"heloo bhi kya haal chall"),
+        React.createElement("img",{className:"pic",style:{width:400,borderRadius:20,backgroundColor:'teal',margin:10},src:'https://tse3.mm.bing.net/th?id=OIP.td23DH_yJ8mggdkkeJmqvgHaHQ&pid=Api&P=0&h=220'},)
+    ),
+    React.createElement("p",{},"heloo bhi kya haal chall"),
+        React.createElement("img",{className:"pic",style:{width:400}, },)
+    )
+)
+
+
+
+
+
+
+
+
+
+
+
+
+const root = ReactDOM.createRoot(document.querySelector("#root"))
+root.render(h2)
