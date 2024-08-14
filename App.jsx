c97f004e01ccb01036c7d5fbed3191c6c1686c34
adivdeco <165708861+adivdeco@users.noreply.github.com>
2024-08-14 22:34:10 +0530

    creating react
    startup

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+    <h1>Control Your Emotions and feelings</h1>
+      
+    </div>
+    
+  )
+}
+
+export default App
---
+++ main.jsx
@@ -0,0 +1,6 @@
+import{createRoot} from "react-dom/client"
+import App from "../components/App"
+console.log("jloo")
+
+const root = createRoot(document.getElementById("root"))
+root.render(<App/>)
